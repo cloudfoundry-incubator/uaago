5ea8d3d73f67c344cc53a19a4e5902d2f14b186c
Warren Fernandes <warren.f.fernandes@gmail.com>
2015-04-21 09:48:20 -0600

    Fixes to sample cli

--- sample_cli/main.go
+++ sample_cli/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	uaa := uaago.NewClient(os.Args[1])
-	token, err := uaa.GetAuthToken(os.Args[2], os.Args[3])
+	token, err := uaa.GetAuthToken(os.Args[2], os.Args[3], false)
 	if err != nil {
 		panic(err.Error())
 	}
